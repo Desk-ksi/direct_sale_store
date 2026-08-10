18b2cf4b2b6e60eb7e10c252f80e6740aa0a000f
daisuke kasai <amtg168bd@gmail.com>
2026-08-10 23:59:24 +0900

    feat: postモデルのバリデーションとアソシエーションを定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  validates :merchandise, length: { maximum: 255 }
+  validates :top_merchandise, length: { maximum: 255 }
+  validates :comment, length: { maximum: 500 }
+  validates :schedule, length: { maximum: 255 }
+  belongs_to :shop
+  has_many :post_images
 end

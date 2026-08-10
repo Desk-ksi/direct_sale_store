abc7a2a126cf942836e04ed9ac453560ff385383
daisuke kasai <amtg168bd@gmail.com>
2026-08-11 00:47:29 +0900

    feat: postモデルに、関連付けしているテーブルのデータが正しいかどうかをチェックするバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   validates :top_merchandise, length: { maximum: 255 }
   validates :comment, length: { maximum: 500 }
   validates :schedule, length: { maximum: 255 }
+  # validates_associated 紐づいているpost_imagesの中身が正しい（壊れていない）かチェックする
+  validates_associated :post_images
   belongs_to :shop
   has_many :post_images
 end

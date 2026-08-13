0c5b1c4f7edf4b884b74bbafae9de05a37796d15
daisuke kasai <amtg168bd@gmail.com>
2026-08-13 23:53:17 +0900

    fix: phone_numberバリデーション正規表現を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :phone_number, presence: true, format: { with: /\A\d{11}\z/ }, uniqueness: true
+  validates :phone_number, presence: true, format: { with: /\A\+\d{12}\z/ }, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true, allow_blank: true
   has_many :user_shops, dependent: :destroy
   has_many :shops, through: :user_shops

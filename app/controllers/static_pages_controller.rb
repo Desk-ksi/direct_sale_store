b13ddb7b9c301bc323f95ffd951f4c4facab57bd
daisuke kasai <amtg168bd@gmail.com>
2026-08-16 03:16:43 +0900

    feat: トップページに最近の投稿10 件が表示されるように定義

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   skip_before_action :require_login
   skip_before_action :require_login_has_shop
-  def top;end
+  def top
+    # 降順（新しいものが上で最初の10件取得）
+    # viewで店舗情報や投稿写真を取得するたびにクエリを発生させないため（N＋1問題の解決）にincledesで一括取得
+    @posts = Post.includes(:shop, :post_images).order(created_at: :desc).first(10)
+  end
 end

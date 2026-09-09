f852804824b9f215b74c5c89795e1ea44a0379b5
daisuke kasai <amtg168bd@gmail.com>
2026-09-10 01:55:37 +0900

    fix: unique規制emailカラムが空文字入る仕様になっていたため修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  before_validation :nullify_blank_email
+
   validates :name, presence: true
   validates :phone_number, presence: true, format: { with: /\A\+\d{12}\z/ }, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true, allow_blank: true
+
   has_many :user_shops, dependent: :destroy
   has_many :shops, through: :user_shops
   has_many :user_shop_likes, dependent: :destroy
@@ -10,4 +13,12 @@ class User < ApplicationRecord
   has_many :user_post_likes, dependent: :destroy
   # 中間テーブルを介して、userがお気に入りにしたpostを参照するときのメソッドを定義。sourceは何を参照するかを明示
   has_many :favorite_posts, through: :user_post_likes, source: :post
+
+  def nullify_blank_email
+    # モデルはどのインスタンスかがわかっているため、カラム名（email）だけで該当の値を取り出せる
+    if email == ""
+      # ローカル変数ではなく、今扱っているインスタンスにメソッドを使っているんだと明示するために、self.が必要
+      self.email = nil
+    end
+  end
 end
